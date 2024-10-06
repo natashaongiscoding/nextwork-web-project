7c78d97163b8b6e16ca99b9d1bc922f927376039
Natasha <natasha@nextwork.org>
2024-10-06 07:48:58 +0000

    already exist

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo yum install tomcat -y
 sudo yum -y install httpd
+echo "Removing existing WAR file if it exists..."
+sudo rm -f /usr/share/tomcat/webapps/nextwork-web-project.war
+echo "Deploying new WAR file..."
 sudo cat < EOF > /etc/httpd/conf.d/tomcat_manager.conf
 <VirtualHost *:80>
   ServerAdmin root@localhost
